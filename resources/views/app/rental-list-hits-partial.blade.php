497bb6470b327bc486e9d0612728cc0330edeff9
thinkontheclock <matthewmacintyre@gmail.com>
2015-09-01 16:43:18 -0300

    CSS Changes: - changed link css to remove blue text
    Markup Changes:
    - wrapped link to rental details around entire list view as opposed to just the address

--- resources/views/app/rental-list-hits-partial.blade.php
+++ resources/views/app/rental-list-hits-partial.blade.php
@@ -3,7 +3,8 @@
         @if($rental->isPromoted())
             <span class="promoted-small"></span>
         @endif
-            <span id="{{ $rental->uuid }}" class="favourite fa {{ in_array($rental->id, $favourites) ? 'fa-heart' : 'fa-heart-o' }}"></span>
+        <span id="{{ $rental->uuid }}" class="favourite fa {{ in_array($rental->id, $favourites) ? 'fa-heart' : 'fa-heart-o' }}"></span>
+        <a href="{{ route('rental.show', [$rental->uuid]) }}" title="Click to view details">
         <div class="images cycle-slideshow" data-cycle-fx="scrollHorz" data-cycle-speed="600" data-cycle-delay="0" data-cycle-timeout="1000">
             @if($rental->photos->count())
                 @foreach($rental->photos as $photo)
@@ -17,8 +18,9 @@
         <span class="availability">Available: {!! $rental->available_at->format('M jS, Y') !!}</span>
         <ul class="listing_attributes">
             <li class="price">${!! $rental->price !!}</li>
-            <li class="address"><a href="{{ route('rental.show', [$rental->uuid]) }}">{!! str_limit($rental->street_address, 20) !!}</a></li>
+            <li class="address">{!! str_limit($rental->street_address, 20) !!}</li>
             <li class="description">{!! ucwords($rental->type) !!} - {!! $rental->beds !!} {!! $rental->beds == 1 ? 'Bed' : 'Beds' !!}</li>
         </ul>
+        </a>
     </li>
 @endforeach
