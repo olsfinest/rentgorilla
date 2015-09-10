9747e053b783faf9dda141343691a52f61969a86
thinkontheclock <matthewmacintyre@gmail.com>
2015-09-10 17:53:15 -0300

    CSS changes: - a few changes to fix the Clear All button in the main search filter (it had a bit too much padding and was slightly misaligned with the rest of the buttons) - more refactoring
    Markup changes:
    - updated included jQuery UI version to latest from Google CDN in an attempt to fix the issue where the filter buttons would get wider every so often on page reload. after spamming F5 for a few minutes to test, the issue seems to be resolved.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
             <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
             <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
             <!--jQuery UI-->
-            <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+            <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
             <script src="/js/auth.js"></script>
             <script src="/js/notifications.js"></script>
             @yield('footer')
