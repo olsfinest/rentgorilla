c2dd4464201eeaf821d868f1ccc502a2a11b5a91
thinkontheclock <matthewmacintyre@gmail.com>
2015-09-14 16:15:24 -0300

    CSS changes: - further refactoring of style.scss - addition of styles for new footer
    Markup changes:
    - added footer.blade.php in partials to produce the new footer content

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,72 @@
+<section class="main">
+    <section class="content full">
+        <section class="social">
+            <ul>
+                <li>Connect with us on: </li>
+                <li><a target="_blank" href="https://www.facebook.com/rentgorilla" title="Connect with RentGorilla on Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" title="Connect with RentGorilla on Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-youtube"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i>+</a></li>
+                <li class="right"><a href="#">Status</a></li>
+                <li class="right"><a href="/terms">Terms of Use</a></li>
+            </ul>
+        </section>
+        <nav>
+            <ul>
+                <li>
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><a href="#" title="View listings from Antigonish, Nova Scotia, in a list"><i class="fa fa-bars"></i> Antigonish Listings</a></li>
+                        <li><a href="#" title="View listings from Antigonish, Nova Scotia, in a map"><i class="fa fa-map-o"></i> Antigonish Listings</a></li>
+                        <li><a href="#"><i class="fa fa-user"></i> My Rentals</a></li>
+                        <li><a href="#"><i class="fa fa-star-o"></i> Favourites</a></li>
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h3>Connect</h3>
+                    <ul>
+                        <li><a href="#" title="Connect with RentGorilla on Facebook"><i class="fa fa-facebook"></i> Facebook</a></li>
+                        <li><a href="#" title="Connect with RentGorilla on Twitter"><i class="fa fa-twitter"></i> Twitter</a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i> YouTube</a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i> Google+</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h3>Quick Links</h3>
+                    <ul>
+                        <li><a href="#" title="View listings from Antigonish, Nova Scotia, in a list"><i class="fa fa-bars"></i> Antigonish Listings</a></li>
+                        <li><a href="#" title="View listings from Antigonish, Nova Scotia, in a map"><i class="fa fa-map-o"></i> Antigonish Listings</a></li>
+                        <li><a href="#"><i class="fa fa-user"></i> My Rentals</a></li>
+                        <li><a href="#"><i class="fa fa-star-o"></i> Favourites</a></li>
+                        <li><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <h3>Connect</h3>
+                    <ul>
+                        <li><a href="#" title="Connect with RentGorilla on Facebook"><i class="fa fa-facebook"></i> Facebook</a></li>
+                        <li><a href="#" title="Connect with RentGorilla on Twitter"><i class="fa fa-twitter"></i> Twitter</a></li>
+                        <li><a href="#"><i class="fa fa-linkedin"></i> LinkedIn</a></li>
+                        <li><a href="#"><i class="fa fa-youtube"></i> YouTube</a></li>
+                        <li><a href="#"><i class="fa fa-google-plus"></i> Google+</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <section class="help">
+            <ul>
+                <li><a href="#"><i class="fa fa-question-circle"></i> Help</a></li>
+                <li><a href="#">RentGorilla</a></li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Terms</a></li>
+                <li class="right"><select name="" id="">
+                    <option value="" selected>English</option>
+                    <option value="">French</option>
+                </select></li>
+            </ul>
+        </section>
+    </section>
+</section>
