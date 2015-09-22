d5d127fcbb818579da8f9bd501a95e2ad64966b1
thinkontheclock <matthewmacintyre@gmail.com>
2015-09-22 17:40:05 -0300

    CSS changes: - zindexing on filter toggle, reverting previous attempts to fix cycle2 slideshow Script changes: - removing script to disable focus on cycle2

--- resources/views/app/list.blade.php
+++ resources/views/app/list.blade.php
@@ -18,21 +18,16 @@
 		</section>
 @endsection
 @section('footer')
-			<!--jQuery-->
+            <!--jQuery-->
 
-        			<!-- Cookies -->
-        		<!--	<script src="/js/jquery-cookie-1.4.1/jquery.cookie.js"></script> -->
-        			<!-- Charts -->
-        			<script type="text/javascript" src="https://www.google.com/jsapi"></script>
-
-        			<!-- Cycle -->
-        			<script src="/js/cycle.js"></script>
-
+            <!-- Cookies -->
+            <!--	<script src="/js/jquery-cookie-1.4.1/jquery.cookie.js"></script> -->
+            <!-- Charts -->
+            <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+			<!-- Cycle -->
+			<script src="/js/cycle.js"></script>
             <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-beta.3/js/select2.min.js"></script>
-        			<!-- Custom JavaScript -->
-        			<!-- <script type="text/javascript" src="/js/min/custom.min.js"></script> -->
-        			<script type="text/javascript" src="/js/list-view.js?v=2"></script>
-                    <script>
-                        $(".images").prop("disabled", true);
-                    </script>
+			<!-- Custom JavaScript -->
+			<!-- <script type="text/javascript" src="/js/min/custom.min.js"></script> -->
+			<script type="text/javascript" src="/js/list-view.js?v=2"></script>
 @endsection
