aa3fe650879c0703f8fd90cf7c07ffeb71e41798
thinkontheclock <matthewmacintyre@gmail.com>
2015-11-18 17:08:06 -0400

    Updating form styles for editing profile and rentals

--- resources/views/rental/edit.blade.php
+++ resources/views/rental/edit.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.admin')
 @section('head')
-    <link rel="stylesheet" href="/css/form.css">
     <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-rc.2/css/select2.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/css/form.css">
 @stop
 @section('content')
     <section class="content full admin">
-        <h1>Edit rental</h1>
+        <h1><i class="fa fa-pencil-square"></i> Edit rental</h1>
         @include('errors.error-list')
         {!! Form::model($rental, ['method' => 'PATCH', 'route' => ['rental.update', $rental->uuid], 'class' => 'form-horizontal', 'id' => 'modify_rental_form']) !!}
         @include('rental.form', ['submitButtonText' => 'Update'])
--- resources/views/rental/form.blade.php
+++ resources/views/rental/form.blade.php
@@ -1,97 +1,103 @@
-    <label for="available" class="">Date Available
-        {!! Form::text('available', null, ['id' => 'available', 'class' => 'form-control', 'readonly', 'placeholder' => 'MM/DD/YYYY (required)']) !!}
-    </label>
-    <label for="street_address" class="">Street Address
-         {!! Form::text('street_address', null, ['class' => 'form-control', 'id' => 'street_address', 'placeholder' => '123 Main Street (required)']) !!}
-    </label>
-    <label for="city" class="">City
-        {!! Form::text('city', $rental->cityOnly, ['class' => 'form-control', 'id' => 'city', 'placeholder' => 'Antigonish (Required)']) !!}
-    </label>
-    <label for="province" class="">Province
-        {!! Form::select('province', Config::get('rentals.provinces'), null, ['id' => 'province', 'class' => 'form-control']) !!}
-    </label>
-    <label for="postal_code" class="">Postal Code
-        {!! Form::text('postal_code', $rental->postal_code, ['id' => 'postal_code', 'class' => 'form-control', 'placeholder' => 'B2G 2L2']) !!}
-    </label>
-    <label for="type" class="">Type
-        {!! Form::select('type', Config::get('rentals.type'), null, ['class' => 'form-control']) !!}
-    </label>
-    <label for="beds" class="">Beds
-        {!! Form::text('beds', null, ['class' => 'form-control', 'placeholder' => '4']) !!}
-    </label>
-    <label for="baths" class="">Bathrooms
-        {!! Form::text('baths', null, ['class' => 'form-control', 'placeholder' => '2.5']) !!}
-    </label>
-    <label for="price" class="">Monthly Rent
-        {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => '500']) !!}
-    </label>
-    <label for="lease" class="">Lease (months)
-        {!! Form::text('lease', null, ['class' => 'form-control', 'placeholder' => '12']) !!}
-    </label>
-    <label for="deposit" class="">Deposit
-        {!! Form::text('deposit', null, ['class' => 'form-control', 'placeholder' => '300']) !!}
-    </label>
-    <label for="laundry" class="">Laundry
-        {!! Form::select('laundry', Config::get('rentals.laundry'), null, ['class' => 'form-control']) !!}
-    </label>
-    <label for="pets" class="">Pets
-        {!! Form::select('pets', Config::get('rentals.pets'), null, ['class' => 'form-control']) !!}
-    </label>
-    <label for="parking" class="">Parking
-        {!! Form::select('parking', Config::get('rentals.parking'), null, ['class' => 'form-control']) !!}
-    </label>
-    <label for="disability_access" class="">Disability Access
-        <span>
-        {!! Form::radio('disability_access', 1) !!} Yes {!! Form::radio('disability_access', 0) !!} No
-        </span>
-    </label>
-    <label for="smoking" class="">Smoking
-        <span>
-        {!! Form::radio('smoking', 1) !!} Yes {!! Form::radio('smoking', 0) !!} No
-        </span>
-    </label>
-    <label for="utilities_included" class="">Utilities Included
-        <span>
-        {!! Form::radio('utilities_included', 1) !!} Yes {!! Form::radio('utilities_included', 0) !!} No
-        </span>
-    </label>
-    <label for="heat_included" class="">Heat Included
-        <span>
-        {!! Form::radio('heat_included', 1) !!} Yes {!! Form::radio('heat_included', 0) !!} No
-        </span>
-    </label>
-    <label for="heats" class="">Heating</label>
-        {!! Form::select('heat_list[]', \RentGorilla\Heat::orderBy('name')->lists('name', 'id')->all(), null, ['id' => 'heats', 'class' => 'form-control', 'multiple']) !!}
-    <br>
-    <br>
-    <label for="furnished" class="">Furnished
-        <span>
+    <fieldset>
+        <legend>
+            <i class="fa fa-map"></i> Location Information
+        </legend>
+        <label for="available" class="">Date Available
+            {!! Form::text('available', null, ['id' => 'available', 'class' => 'form-control', 'readonly', 'placeholder' => 'MM/DD/YYYY (required)']) !!}
+        </label>
+        <label for="street_address" class="half left">Street Address <i title="Feel free to use street abbreviations and specify apartment or unit numbers." class="fa fa-info-circle"></i> 
+             {!! Form::text('street_address', null, ['class' => 'form-control', 'id' => 'street_address', 'placeholder' => '123 Main Street (required)']) !!}
+        </label>
+        <label for="city" class="half right">City
+            {!! Form::text('city', $rental->cityOnly, ['class' => 'form-control', 'id' => 'city', 'placeholder' => 'Antigonish (Required)']) !!}
+        </label>
+        <label for="province" class="half left">Province
+            {!! Form::select('province', Config::get('rentals.provinces'), null, ['id' => 'province', 'class' => 'form-control']) !!}
+        </label>
+        <label for="postal_code" class=" half right">Postal Code
+            {!! Form::text('postal_code', $rental->postal_code, ['id' => 'postal_code', 'class' => 'form-control', 'placeholder' => 'B2G 2L2']) !!}
+        </label>
+    </fieldset>
+    <fieldset>
+        <legend><i class="fa fa-list-alt"></i> Property Details</legend>
+        <label for="type" class="half left">Type
+            {!! Form::select('type', Config::get('rentals.type'), null, ['class' => 'form-control']) !!}
+        </label>
+        <label for="beds" class="half right">Bedrooms
+            {!! Form::text('beds', null, ['class' => 'form-control', 'placeholder' => '4']) !!}
+        </label>
+        <label for="baths" class="half left">Bathrooms <i class="fa fa-info-circle" title="Indicating a half bathroom means a sink and a toilet but no bath or shower"></i>
+            {!! Form::text('baths', null, ['class' => 'form-control', 'placeholder' => '2.5']) !!}
+        </label>
+        <label for="price" class="half right">Monthly Rent <i class="fa fa-info-circle" title="This value should reflect the total unit cost. Please note in your description if the price is per room."></i>
+            {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => '500']) !!}
+        </label>
+        <label for="lease" class="half left">Lease (months)
+            {!! Form::text('lease', null, ['class' => 'form-control', 'placeholder' => '12']) !!}
+        </label>
+        <label for="deposit" class="half right">Deposit
+            {!! Form::text('deposit', null, ['class' => 'form-control', 'placeholder' => '300']) !!}
+        </label>
+        <label for="laundry" class="half left">Laundry
+            {!! Form::select('laundry', Config::get('rentals.laundry'), null, ['class' => 'form-control']) !!}
+        </label>
+        <label for="pets" class="half right">Pets
+            {!! Form::select('pets', Config::get('rentals.pets'), null, ['class' => 'form-control']) !!}
+        </label>
+        <label for="parking" class="half left">Parking
+            {!! Form::select('parking', Config::get('rentals.parking'), null, ['class' => 'form-control']) !!}
+        </label>
+        <label for="disability_access" class="half right">Disability Access
+            <span class="labelSpan">
+            {!! Form::radio('disability_access', 1) !!} Yes {!! Form::radio('disability_access', 0) !!} No
+            </span>
+        </label>
+        <label for="smoking" class="half left">Smoking
+            <span class="labelSpan">
+            {!! Form::radio('smoking', 1) !!} Yes {!! Form::radio('smoking', 0) !!} No
+            </span>
+        </label>
+        <label for="utilities_included" class="half right">Utilities Included
+            <span class="labelSpan">
+            {!! Form::radio('utilities_included', 1) !!} Yes {!! Form::radio('utilities_included', 0) !!} No
+            </span>
+        </label>
+        <label for="furnished" class="half left">Furnished
+        <span class="labelSpan">
         {!! Form::radio('furnished', 1) !!} Yes {!! Form::radio('furnished', 0) !!} No
         </span>
     </label>
-    <label for="square_footage" class="">Square Footage
+    <label for="square_footage" class="half right">Square Footage
         {!! Form::text('square_footage', null, ['class' => 'form-control', 'placeholder' => '1000']) !!}
-    </label>
-    <label for="video" class="">Link to video
-        {!! Form::text('video', null, ['class' => 'form-control', 'placeholder' => 'https://www.youtube.com/watch?v=764xqeDO3yk']) !!}
-    </label>
-    <label for="description" class="">Description
-        {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'A brief description of the property...']) !!}
     </label>
     <label for="features" class="">Features</label>
         {!! Form::select('feature_list[]', \RentGorilla\Feature::orderBy('name')->lists('name', 'id')->all(), null, ['id' => 'features', 'class' => 'form-control', 'multiple']) !!}
 
     <label for="appliances" class="">Appliances</label>
         {!! Form::select('appliance_list[]', \RentGorilla\Appliance::orderBy('name')->lists('name', 'id')->all(), null, ['id' => 'appliances', 'class' => 'form-control', 'multiple']) !!}
-    <br>
-
+    </fieldset>
+    <fieldset>
+        <legend><i class="fa fa-fire"></i> Heating</legend>
+        <label for="heat_included" class="">Heat Included
+            <span class="labelSpan">
+            {!! Form::radio('heat_included', 1) !!} Yes {!! Form::radio('heat_included', 0) !!} No
+            </span>
+        </label>
+        <label for="heats" class="">Heating</label>
+            {!! Form::select('heat_list[]', \RentGorilla\Heat::orderBy('name')->lists('name', 'id')->all(), null, ['id' => 'heats', 'class' => 'form-control', 'multiple']) !!}
+    </fieldset>
+    <fieldset>
+        <legend><i class="fa fa-question-circle"></i> Vitals</legend>
+        <label for="video" class="">Link to video
+            {!! Form::text('video', null, ['class' => 'form-control', 'placeholder' => 'https://www.youtube.com/watch?v=764xqeDO3yk']) !!}
+        </label>
+        <label for="description" class="">Description
+            {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'A brief description of the property...']) !!}
+        </label>
+    </fieldset>
     <label for="active" class="">Activate Rental</label>
         {!! Form::checkbox('active') !!}
-
     {!! Form::submit($submitButtonText) !!}
-
-
-
 @section('footer')
     <script src="/js/select2.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js"></script>
