c1ae91301399ca615ce1dc0011f45f205af94b8e
Curran Jensen <curran.jensen@gmail.com>
2016-03-09 19:46:59 -0400

    Changed wording on achievement awarded email

--- resources/views/emails/user/achievement-awarded.blade.php
+++ resources/views/emails/user/achievement-awarded.blade.php
@@ -3,5 +3,6 @@
 @section('body')
     <p>Congratulations! You have earned the achievement badge <strong>{{ $achievement }}</strong>!</p>
     <p><strong>{{ $points }}</strong> points have been applied to your account.</p>
-    <p>You now have <strong>{{ $total }}</strong> points, which can be exchanged for credit on your next bill if you have an active subscription!</p>
+    <p>You now have <strong>{{ $total }}</strong> points, which can be exchanged for credit on your next bill if you have a minimum of 10,000 points and an active subscription!</p>
+    <p>Click <a href="https://rentgorilla.ca/admin/subscription/plan">here</a> to learn more about how you can earn more points every month.</p>
 @stop
