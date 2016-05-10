3a9e532244595421cdce9c98bc422c1a11d213c6
Curran Jensen <curran.jensen@gmail.com>
2016-05-10 20:22:07 -0300

    Add Facebook sharing metadata for property details page

--- resources/views/rental/twitter-summary-large-image.blade.php
+++ resources/views/rental/twitter-summary-large-image.blade.php
@@ -1,7 +1,22 @@
 @if($rental->photos()->count())
+<?php $title = $rental->street_address . ' ' . $rental->location->city . ', ' . Config::get('rentals.provinces.' . $rental->location->province); ?>
+<?php $description = $rental->beds . ' Bedroom / ' . (float) $rental->baths . ' Bathroom ' .  Config::get('rentals.type.' . $rental->type); ?>
+<?php $image = $rental->photos->first(); ?>
+<?php $mediumImage = url($image->getSize('medium')); ?>
+<?php $largeImage = url($image->getSize('large')); ?>
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@rentgorillaca">
-<meta name="twitter:title" content="{{ $rental->street_address . ' ' . $rental->location->city . ', ' . Config::get('rentals.provinces.' . $rental->location->province) }} | RentGorilla.ca">
-<meta name="twitter:description" content="{{ $rental->beds }} Bedroom / {{ (float) $rental->baths }} Bathroom {{ Config::get('rentals.type.' . $rental->type) }}">
-<meta name="twitter:image" content="{{ url($rental->photos->first()->getSize('medium')) }}">
+<meta name="twitter:title" content="{{ $title }} | RentGorilla.ca">
+<meta name="twitter:description" content="{{ $description }}">
+<meta name="twitter:image" content="{{ $mediumImage }}">
+<meta property="og:url" content="{{ route('rental.show', $rental->uuid) }}" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="{{ $title }}" />
+<meta property="og:description" content="{{ $description }}" />
+<meta property="og:site_name" content="RentGorilla" />
+<meta property="og:image" content="{{ $largeImage }}" />
+<meta property="og:image:url" content="{{ $largeImage }}" />
+<meta property="og:image:secure_url" content="{{ $largeImage }}" />
+<meta property="og:image:width" content="{{ \RentGorilla\Photo::LARGE_WIDTH }}" />
+<meta property="og:image:height" content="{{ \RentGorilla\Photo::LARGE_HEIGHT }}" />
 @endif
