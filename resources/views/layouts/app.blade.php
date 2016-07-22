2a312282dfb059d5f0decdc2e5906be0247370dd
Matt MacIntyre <matthewmacintyre@gmail.com>
2016-07-22 12:24:25 -0300

    Hang fix attempt #3 - added font awesome files to be hosted locally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
         <meta name="description" content="Discover rental listings from all over Nova Scotia. Add your rental property for free.">
         <!-- <link href="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0-beta.3/css/select2.min.css" rel="stylesheet" /> -->
     <link href="/css/select2.min.css" rel="stylesheet" />
-		<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet">
+		<!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet"> -->
+		<link href="/css/fa/css/font-awesome.min.css" rel="stylesheet">
 		<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css" />
 		<link rel="stylesheet" type="text/css" href="/css/typography.css">
 		<link rel="stylesheet" type="text/css" href="/css/jquery-ui.theme.css">
